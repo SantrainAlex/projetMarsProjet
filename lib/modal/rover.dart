857aba63a169084894f6e6dd6dcb0beb69745ab8
alex santrain <alexissantrain@orangefr>
2022-09-08 14:42:43 +0200

    Partie 2

--- lib/modal/rover.dart
+++ lib/modal/rover.dart
@@ -1,19 +1,44 @@
+import 'dart:ffi';
+
+import 'direction.dart';
+
 class Rover {
   int positionX;
   int positionY;
   final int maxX;
   final int maxY;
+  Direction direction;
 
   Rover(
-      {this.positionX = 0, this.positionY = 0, this.maxX = 10, this.maxY = 10});
+      {this.positionX = 0,
+      this.positionY = 0,
+      this.maxX = 10,
+      this.maxY = 10,
+      this.direction = Direction.Nord});
 
   int deplacementY(int value) {
-    positionY = positionY + (value);
-    return positionY;
+    if (this.direction == Direction.Nord) {
+      positionY = positionY + value;
+      return positionY;
+    } else if (this.direction == Direction.Sud) {
+      positionY = positionY - value;
+      return positionY;
+    }
+    return 0;
   }
 
   int deplacementX(int value) {
-    positionX = positionX + (value);
-    return positionX;
+    if (this.direction == Direction.Est) {
+      positionX = positionX + value;
+      return positionX;
+    } else if (this.direction == Direction.Ouest) {
+      positionX = positionX - value;
+      return positionX;
+    }
+    return 0;
+  }
+
+  move(Direction direction) {
+    this.direction = direction;
   }
 }
--- test/mars_rover_test.dart
+++ test/mars_rover_test.dart
@@ -1,4 +1,5 @@
 import 'package:mars_rover/mars_rover.dart';
+import 'package:mars_rover/modal/direction.dart';
 import 'package:mars_rover/modal/rover.dart';
 import 'package:test/test.dart';
 
@@ -18,31 +19,52 @@ void main() {
       expect(rover.maxY, 10);
     });
   });
-
+  group("Test vers quel direction le rover regarde", () {
+    test("Test vers quel direction le rover regarde (Nord)", () {
+      Rover rover = Rover();
+      rover.direction = Direction.Nord;
+      expect(rover.direction, Direction.Nord);
+    });
+    test("Test vers quel direction le rover regarde (Est)", () {
+      Rover rover = Rover();
+      rover.direction = Direction.Est;
+      expect(rover.direction, Direction.Est);
+    });
+    test("Test vers quel direction le rover regarde (Sud)", () {
+      Rover rover = Rover();
+      rover.move(Direction.Sud);
+      expect(rover.direction, Direction.Sud);
+    });
+    test("Test vers quel direction le rover regarde (Ouest)", () {
+      Rover rover = Rover();
+      rover.move(Direction.Ouest);
+      expect(rover.direction, Direction.Ouest);
+    });
+  });
   group("Deplacement du Rover", () {
     test("Test de l'axe Y (Avancer)", () {
       Rover rover = Rover();
-
+      rover.direction = Direction.Nord;
       expect(rover.deplacementY(1), 1);
       expect(rover.positionX, 0);
     });
     test("Test de l'axe Y (Reculer)", () {
       Rover rover = Rover();
-
-      expect(rover.deplacementY(-1), -1);
+      rover.direction = Direction.Sud;
+      expect(rover.deplacementY(1), -1);
       expect(rover.positionX, 0);
     });
 
     test("Test de l'axe X (Avancer)", () {
       Rover rover = Rover();
-
+      rover.direction = Direction.Est;
       expect(rover.deplacementX(1), 1);
       expect(rover.positionY, 0);
     });
     test("Test de l'axe X (Reculer)", () {
       Rover rover = Rover();
-
-      expect(rover.deplacementX(-1), -1);
+      rover.direction = Direction.Ouest;
+      expect(rover.deplacementX(1), -1);
       expect(rover.positionY, 0);
     });
   });
